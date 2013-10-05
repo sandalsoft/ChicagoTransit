1a9ca7bbe7639abaca523f23e0a94c1af50b5a43
Eric Nelson <eric@sandalsoft.com>
2013-10-05 09:46:51 -0500

    Initial commit.  AFnetworking working, XMLDictionary parsing

--- ChicagoTransit/ViewController.m
+++ ChicagoTransit/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "XMLDictionary.h"
+#import <AFNetworking/AFNetworking.h>
+#import <AFNetworking/AFHTTPRequestOperationManager.h>
+
 
 @interface ViewController ()
 
@@ -18,6 +22,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self doStuff];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +31,24 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+- (void)doStuff {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//    manager.responseSerializer = [AFXMLParserResponseSerializer serializer];
+    [manager GET:@"http://www.ctabustracker.com/bustime/api/v1/getroutes"
+      parameters:@{@"key":@"L8AhtAaqytXUu3tjDxsjyThrQ"}
+         success:^(AFHTTPRequestOperation *operation, id responseObject) {
+             XMLDictionaryParser *xmlParser = [[XMLDictionaryParser alloc] init];
+             
+             NSDictionary *data = [NSDictionary dictionaryWithDictionary:[xmlParser dictionaryWithData:responseObject]];
+             NSLog(@"XML: %@", [data description] );
+       } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"Error: %@", error);
+       }];
+}
+
 
 @end
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '7.0'
+pod 'AFNetworking', :git => 'https://github.com/AFNetworking/AFNetworking', :tag => '2.0.0-RC3'
+pod 'Reachability',  '~> 3.0.0'
+pod 'XMLDictionary'
+
